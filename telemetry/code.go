213e0a5f57a1bce24d07dcadecb7c784b1506f6a
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-07-31 16:20:49 +0200

    feat: use foomo.go CallFrame for code attributes

--- telemetry/code.go
+++ telemetry/code.go
@@ -1,23 +1,11 @@
 package telemetry
 
 import (
-	runtimex "github.com/foomo/go/runtime"
+	goruntime "github.com/foomo/go/runtime"
 	"go.opentelemetry.io/otel/attribute"
 	semconv "go.opentelemetry.io/otel/semconv/v1.41.0"
 )
 
-func CodeCaller(skip int) []attribute.KeyValue {
-	if shortName, _, file, line, ok := runtimex.Caller(skip + 1); ok {
-		return []attribute.KeyValue{
-			semconv.CodeFunctionName(shortName),
-			semconv.CodeFilePath(file),
-			semconv.CodeLineNumber(line),
-		}
-	}
-
-	return nil
-}
-
 func CodeStacktrace(num, skip int) attribute.KeyValue {
-	return semconv.CodeStacktrace(runtimex.StackTrace(num, skip+1))
+	return semconv.CodeStacktrace(goruntime.StackTrace(num, skip+1))
 }
